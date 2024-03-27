b25eb02f8a03083abf0a85e0b1eb641058fd8578
Mary Melnikova <marymelnikova1988@gmail.com>
2024-03-27 22:16:40 +0300

    24.2 фильтрация

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics, filters
-from django_filters import OrderingFilter
+from rest_framework import generics
+
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 
 
@@ -30,11 +31,9 @@ class UserCreateAPIView(generics.CreateAPIView):
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
-    # filter_backends = [OrderingFilter, SearchFilter]
-    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
-    # filter_backends = [DjangoFilterBackend, OrderingFilter]
-    # filterset_fields = ('paid_course', 'paid_lesson', 'pay_method')
-    # ordering_fields = ('payment_date',)
-    ordering_fields = ['payment_date']
-    search_fields = ['paid_course', 'paid_lesson', 'pay_method']
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('paid_course', 'paid_lesson', 'pay_method')
+    ordering_fields = ('payment_date',)
+
+
 
