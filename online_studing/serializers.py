79ec9acfb534f55c1fdcdc738b33bbf7e57dd112
Mary Melnikova <marymelnikova1988@gmail.com>
2024-04-02 00:21:57 +0300

    25.2 Валидаторы, пагинация и тесты

--- online_studing/serializers.py
+++ online_studing/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 from online_studing.models import Course, Lesson
+from online_studing.validators import UrlValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [UrlValidator(field='video_url')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
